58066d5f3ce8d6d0d13bc580cdad35c32ee03943
adnapp <adam.napps@gmail.com>
2020-10-28 10:44:07 -0400

    commit tree cc770ee896a7e786b5c5e80319b3d1bd6019e28a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,36 @@
 # your code goes here
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+   def clean?
+     @hygiene > 7
+   end
+
+   def happy?
+    @happiness > 7
+   end
+
+   def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+   end
+
+
+end
+
+
+
+ # def happiness=(happiness)
+    #     while happiness <=0 $$ happiness >=10
+    #         happiness += 1
+    #     end
+    # end
